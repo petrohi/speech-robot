39d813a0094521db3bf47414bbcd60450574545e
Peter Hizalev <peter.hizalev@gmail.com>
2022-06-21 22:03:44 -0700

    Generalize adcs747x_to_axism module

--- vivado/adcs747x_to_axism.v
+++ vivado/adcs747x_to_axism.v
@@ -1,6 +1,13 @@
 `timescale 1ps/1ps
 
-module adcs747x_to_axism(
+module adcs747x_to_axism
+    #(
+    parameter DATA_WIDTH = 16,
+    parameter PACKET_SIZE = 128,
+    parameter SPI_SCK_DIV = 200
+)
+    
+    (
     output wire SPI_SSN,
     output wire SPI_SCK,
     input wire SPI_MISO,
@@ -12,72 +19,74 @@ module adcs747x_to_axism(
     output wire M_AXIS_TLAST,
     input wire M_AXIS_TREADY
 );
-    reg [7:0] spi_sck_counter;
+    localparam SPI_SCK_PERIOD_DIV = SPI_SCK_DIV / 2;
+
+    reg [$clog2(SPI_SCK_PERIOD_DIV) - 1:0] spi_sck_counter;
     reg spi_sck;
     reg spi_sck_last;
 
-    reg [3:0] spi_ssn_counter;
+    reg [$clog2(DATA_WIDTH) - 1:0] spi_ssn_counter;
     reg spi_ssn;
     reg spi_ssn_last;
 
-    reg [15:0] spi_sample;
+    reg [DATA_WIDTH - 1:0] spi_sample;
 
-    reg [15:0] m_axis_tdata;
+    reg [DATA_WIDTH - 1:0] m_axis_tdata;
 
     reg m_axis_tvalid;
     reg m_axis_tlast;
 
-    reg [7:0] packet_counter;
+    reg [$clog2(PACKET_SIZE) - 1:0] packet_counter;
 
     always@(posedge AXIS_ACLK) begin
         if(!AXIS_ARESETN) begin
             spi_ssn_counter <= 0;
             spi_ssn <= 0;
             spi_ssn_last <= 0;
-            
+
             spi_sck_counter <= 0;
             spi_sck <= 0;
-            spi_sck_last <= 0;            
+            spi_sck_last <= 0;
 
             m_axis_tvalid <= 1'b0;
             m_axis_tlast <= 1'b0;
-            m_axis_tdata <= 16'b0;
-            
+            m_axis_tdata <= {DATA_WIDTH{1'b0}};
+
             packet_counter <= 0;
         end else begin
-            if (spi_sck_counter == 99) begin
+            if (spi_sck_counter == SPI_SCK_PERIOD_DIV - 1) begin
                 spi_sck <= !spi_sck;
                 spi_sck_counter <= 0;
             end else begin
                 spi_sck_counter <= spi_sck_counter + 1;
             end
-            
+
             if (!spi_sck_last && spi_sck) begin
-                spi_sample <= {spi_sample[14:0], SPI_MISO};
+                spi_sample <= {spi_sample[DATA_WIDTH - 2:0], SPI_MISO};
             end
 
             if (spi_sck_last && !spi_sck) begin
-                if (spi_ssn_counter == 15) begin
+                if (spi_ssn_counter == DATA_WIDTH - 1) begin
                     spi_ssn <= !spi_ssn;
                     spi_ssn_counter <= 0;
                 end else begin
                     spi_ssn_counter <= spi_ssn_counter + 1;
-                end            
+                end
             end
 
             if (!spi_ssn_last && spi_ssn) begin
                 m_axis_tvalid <= 1'b1;
                 m_axis_tlast <= 1'b0;
-            
+
                 if (M_AXIS_TREADY) begin
-                    if (packet_counter == 127) begin
+                    if (packet_counter == PACKET_SIZE - 1) begin
                         m_axis_tlast <= 1'b1;
                         packet_counter <= 0;
                     end else begin
                         packet_counter <= packet_counter + 1;
                     end
-                end                    
-                    
+                end
+
                 m_axis_tdata <= spi_sample;
             end else begin
                 m_axis_tvalid <= 1'b0;
@@ -86,7 +95,7 @@ module adcs747x_to_axism(
 
             spi_ssn_last <= spi_ssn;
             spi_sck_last <= spi_sck;
-        end                
+        end
     end
 
     assign SPI_SCK = spi_sck;
